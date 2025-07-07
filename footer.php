e81e912e5583d2f9ca4f4841d3b6918f08670e21
MilennaAbreu <134614543+MilennaAbreu@users.noreply.github.com>
2025-07-07 17:01:20 -0300

    Add store map page

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
       <div class="footer-links">
         <a href="#">Ajuda</a>
         <a href="#">Contato</a>
-        <a href="#">Localizar Loja</a>
+        <a href="localizar_loja.php">Localizar Loja</a>
       </div>
       <p>&copy; <?php echo date('Y'); ?> Óticas Alamanda. Todos os direitos reservados.</p>
     </div>
