d970f5b545ee64d44490cb8edc682078c8c09d95
MilennaAbreu <134614543+MilennaAbreu@users.noreply.github.com>
2025-07-07 17:17:56 -0300

    Improve slider sizing and fix map embed

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
         <a href="#">Contato</a>
       </div>
       <div class="map-container">
-        <iframe src="https://maps.app.goo.gl/URxTHAm8oKWCdQp29" allowfullscreen loading="lazy"></iframe>
+        <iframe src="https://maps.google.com/maps?q=Óticas+Alamanda&output=embed" allowfullscreen loading="lazy"></iframe>
         <p><a href="https://maps.app.goo.gl/URxTHAm8oKWCdQp29" target="_blank" rel="noopener">Abrir no Google Maps</a></p>
       </div>
       <p>&copy; <?php echo date('Y'); ?> Óticas Alamanda. Todos os direitos reservados.</p>
