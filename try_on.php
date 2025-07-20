907c0fc5b18b283653e9462dee2ef605c996fedd
MilennaAbreu <134614543+MilennaAbreu@users.noreply.github.com>
2025-07-19 22:38:39 -0300

    Update try-on model to OBJ

--- try_on.php
+++ try_on.php
@@ -6,11 +6,11 @@ include 'header.php';
   <div id="ar-container">
     <a-scene mindar-face embedded color-space="sRGB" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
       <a-assets>
-        <a-asset-item id="glassesModel" src="assets/images/glasses-1-.glb"></a-asset-item>
+        <a-asset-item id="glassesObj" src="assets/images/Glasses.obj"></a-asset-item>
       </a-assets>
       <a-camera active="false" position="0 0 0"></a-camera>
       <a-entity mindar-face-target="anchorIndex: 168">
-        <a-gltf-model rotation="0 0 0" position="0 -0.4 0.5" scale="0.75 0.75 0.75" src="#glassesModel"></a-gltf-model>
+        <a-entity obj-model="obj: #glassesObj" rotation="0 0 0" position="0 -0.4 0.5" scale="0.75 0.75 0.75"></a-entity>
       </a-entity>
     </a-scene>
   </div>
